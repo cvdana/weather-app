a38eed382f0974c3e3ff9de7e0e9b91f14fa4c1a
cvdana <cvdana99@gmail.com>
2023-03-16 13:47:10 +0100

    Changes in style

--- src/javascript.js
+++ src/javascript.js
@@ -149,12 +149,3 @@ function displayForecast(response) {
   forecastElemant.innerHTML = forecastHTML;
 }
 displayForecast();
-const triggerTabList = document.querySelectorAll("#myTab button");
-triggerTabList.forEach((triggerEl) => {
-  const tabTrigger = new bootstrap.Tab(triggerEl);
-
-  triggerEl.addEventListener("click", (event) => {
-    event.preventDefault();
-    tabTrigger.show();
-  });
-});
